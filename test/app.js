436ad9ff9683c3704e7fc8517a17c83e50068b2c
Nate Lang <lang.na@husky.neu.edu>
2017-07-05 15:30:36 -0400

    Changed to work with MLab as well

--- test/app.js
+++ test/app.js
@@ -6,14 +6,12 @@ app.post("/api/test", createMessage);
 app.delete("/api/test/:id", deleteMessage);
 
 var connectionString = 'mongodb://127.0.0.1:27017/test'; // for local
-if(process.env.MLAB_USERNAME_WEBDEV) { // check if running remotely
-    var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
-    var password = process.env.MLAB_PASSWORD_WEBDEV;
+if(process.env.MONGO_USER){ // check if running remotely
+    var username = process.env.MONGO_USER; // get from environment
+    var password = process.env.MONGO_PASS;
     connectionString = 'mongodb://' + username + ':' + password;
-    connectionString += '@ds157268.mlab.com:57268/heroku_nh37fqq4'; // user yours
+    connectionString += '@ds013192.mlab.com:13192/heroku_q9g26xqw'; // user yours
 }
-// Replace "@ds157268.mlab.com:57268/heroku_nh37fqq4"
-// above with your own URL given to you by mLab
 
 var mongoose = require("mongoose");
 mongoose.connect(connectionString);
